b0b5f1e00a7de48c22beb1a584bfd9b9512f862b
matan844 <matan3484@gmail.com>
2023-08-21 22:39:56 +0300

    new

---
+++ server.js
@@ -0,0 +1,16 @@
+// server.js
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const apiRoutes = require('./weatherRout');
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+app.use(cors());
+app.use(bodyParser.json());
+app.use('/api', apiRoutes);
+// Define your API routes here
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
---
+++ weatherRout.js
@@ -0,0 +1,31 @@
+const express = require("express");
+const router = express.Router();
+const axios = require("axios");
+
+// Define your API routes here
+router.get("/data", async (req, res) => {
+  try {
+    const { city } = req.query;
+    if (!city) {
+      return res.status(400).json({ error: "City parameter is missing." });
+    }
+    const response = await axios.get(
+      `http://api.weatherapi.com/v1/current.json?key=90e9aa3641dd4b31a09190503231208&q=${city}&aqi=no`
+    );
+    const responseData = {
+      city:response.data.location.name ,
+      celtemp: response.data.current.temp_c,
+      wind_kph: response.data.current.wind_kph,
+      precip_mm: response.data.current.precip_mm,
+      humidity: response.data.current.humidity,
+      country : response.data.location.country
+    };
+
+    res.json(responseData);
+  } catch (error) {
+    console.error("Error fetching city information:", error);
+    res.status(500).json({ error: "Error fetching city information" });
+  }
+});
+
+module.exports = router;
